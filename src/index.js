f18d88e6403947b0fb43bdc0827b5237a21f9508
Caroline <caroline.syokau@student.moringaschool.com>
2022-11-29 21:07:49 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
+
+  document.addEventListener('DOMContentLoaded', ()=> {
+    
+   let form = document.querySelector('form')
+   form.addEventListener('submit',(e) => {
+   
+    e.preventDefault()
+    buildToDos(e.target)
+    form.reset()
+   }) 
+})
   // your code here
-});
+
+  function buildToDos(form){
+    let toDo = form.new_task_description.value
+    let toDos = document.getElementById('tasks')
+    let li = document.createElement('li')
+    li.textContent = toDo
+    toDos.appendChild(li)
+  }
